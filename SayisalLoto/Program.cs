411cddc60e67aff3f20b51f6e80417bcc90886fe
ASUS <ASUS@DESKTOP-GQND9IH>
2023-01-23 10:13:26 +0300

    Add project files.

---
+++ SayisalLoto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayisalLoto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-49 arasında tekrar etmeyen 6 adet random sayı üretir
+
+            Random rnd = new Random();
+            int sayi;
+            int[] sayisalLoto = new int[6];
+
+            for (int i = 0; i < sayisalLoto.Length;)
+            {
+                sayi = rnd.Next(1, 50);
+
+                if (!sayisalLoto.Contains(sayi))
+                {
+                    sayisalLoto[i] = sayi;
+                    i++;
+                }
+            }
+
+            Array.Sort(sayisalLoto);
+            foreach (var k in sayisalLoto)
+            {
+                Console.Write(k + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
